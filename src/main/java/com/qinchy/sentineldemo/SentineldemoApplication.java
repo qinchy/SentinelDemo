a58e4abae34c30e3410e4380743ae69dd5269f03
qinchy <qinchy@msn.com>
2018-09-30 08:59:18 +0800

    init

---
+++ src/main/java/com/qinchy/sentineldemo/SentineldemoApplication.java
@@ -0,0 +1,12 @@
+package com.qinchy.sentineldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentineldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentineldemoApplication.class, args);
+    }
+}
