1703f7e772a2f636be222aedb718e72136f84486
qinchy <qinchy@msn.com>
2018-09-30 15:34:04 +0800

    init

---
+++ src/main/java/com/qinchy/sentineldemo/controller/SentinelController.java
@@ -0,0 +1,15 @@
+package com.qinchy.sentineldemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class SentinelController {
+
+    @GetMapping("/flowctl")
+    public String sentinel(){
+        return "success";
+    }
+}
